daf445a5abfbe43066a0c18b2fc56d3d1562d055
Okish04 <obanoashley87@gmail.com>
2024-09-04 04:53:36 +0300

    Update

---
+++ Hello.js
@@ -0,0 +1,13 @@
+// pages/hello.js
+import React from 'react';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello, Welcome to My Toy Shop!</h1>
+      <p>We have the best toys in town!</p>
+    </div>
+  );
+}
+
+export default Hello;
